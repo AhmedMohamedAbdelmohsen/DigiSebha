6c396e2c8e2ec849dedabbb2e2bb8321d96cee75
ahmedmohamedabdelmohsen96@yahoo.com <b7bbabagamedyasoyoyo>
2020-03-18 04:52:06 +0200

    DigiSebha

--- app/src/main/java/com/example/digisebhaa/RosaryFragment.java
+++ app/src/main/java/com/example/digisebhaa/RosaryFragment.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,6 +29,8 @@ public class RosaryFragment extends Fragment {
     private Typeface typeface;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private Vibrator vibrator;
+    private ToggleButton toggleButton;
 
     public RosaryFragment() {
         // Required empty public constructor
@@ -48,8 +52,12 @@ public class RosaryFragment extends Fragment {
         BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottom_nav_bar);
         bottomNavigationView.setVisibility(View.VISIBLE);
 
+        vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
         ImageButton imageButton = getActivity().findViewById(R.id.btn_exit);
         imageButton.setVisibility(View.VISIBLE);
+
+        binding.tgbtnVibration.setChecked(true);
 
         typeface = Typeface.createFromAsset(getActivity().getAssets(), "almushaf.ttf");
         binding.tvTitle.setTypeface(typeface);
@@ -61,6 +69,12 @@ public class RosaryFragment extends Fragment {
         binding.fabCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //setVibrator(vibrator);
+                if (binding.tgbtnVibration.isChecked()) {
+                    vibrator.vibrate(100);
+                } else {
+                    vibrator.cancel();
+                }
                 int x = Integer.parseInt(binding.tvCounter.getText().toString());
                 if (x < 1000000) {
                     x++;
@@ -70,12 +84,18 @@ public class RosaryFragment extends Fragment {
                 editor.putInt("counter", x);
                 editor.apply();
                 binding.tvCounter.setText(String.valueOf(x));
+
             }
         });
 
         binding.fabResetCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (binding.tgbtnVibration.isChecked()) {
+                    vibrator.vibrate(300);
+                } else {
+                    vibrator.cancel();
+                }
                 int y = sharedPreferences.getInt("counter", 0);
                 if (y == 0) {
                     Toast.makeText(v.getContext(), "عدد التسبيحات 0 بالفعل", Toast.LENGTH_SHORT).show();
@@ -89,4 +109,5 @@ public class RosaryFragment extends Fragment {
         });
 
     }
+
 }
